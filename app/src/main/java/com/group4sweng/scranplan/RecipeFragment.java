6dc5d3f3d2432dbb0146fd6d7d440df7f03dab42
Nathan Billis <nathanbillis.nathan@gmail.com>
2020-02-06 14:23:12 +0000

    generates content for views
    now with scrolling!

--- app/src/main/java/com/group4sweng/scranplan/RecipeFragment.java
+++ app/src/main/java/com/group4sweng/scranplan/RecipeFragment.java
@@ -1,11 +1,17 @@
 package com.group4sweng.scranplan;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -25,15 +31,29 @@ public class RecipeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recipe, container, false);
 
-        LinearLayout linearLayout = view.findViewById(R.id.linearLayout);
-        for (int i =0; i < 10; i++) {
-            ImageView imageView = new ImageView(view.getContext());
-            imageView.setId(i);
-            imageView.setAdjustViewBounds(true);
-            imageView.setPadding(10,10,10,10);
-            imageView.setImageResource(R.drawable.scran);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            linearLayout.addView(imageView);
+        LinearLayout verticalLayout = view.findViewById(R.id.linearLayout);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+
+        for (int i=0; i < 10; i++) {
+            TextView mTextView = new TextView(view.getContext());
+            mTextView.setText(Integer.toString(height));
+            HorizontalScrollView horizontalScrollView = new HorizontalScrollView(view.getContext());
+            horizontalScrollView.setLayoutParams(new ViewGroup.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, height/5));
+            LinearLayout horizontalLayout = new LinearLayout(view.getContext());
+            horizontalScrollView.addView(horizontalLayout);
+            for (int j =0; j < 10; j++) {
+                ImageView imageView = new ImageView(view.getContext());
+                imageView.setId(j);
+                imageView.setAdjustViewBounds(true);
+                imageView.setPadding(10,10,10,10);
+                imageView.setImageResource(R.drawable.scran);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                horizontalLayout.addView(imageView);
+            }
+            verticalLayout.addView(mTextView);
+            verticalLayout.addView(horizontalScrollView);
         }
         return view;
     }
