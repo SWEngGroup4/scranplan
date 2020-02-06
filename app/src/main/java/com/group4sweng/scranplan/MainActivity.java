e2fe5eb643211e8969e48b21c63fe9168af077e1
Nathan Billis <nathanbillis.nathan@gmail.com>
2020-02-06 13:43:32 +0000

    adds side bar functionality

--- app/src/main/java/com/group4sweng/scranplan/MainActivity.java
+++ app/src/main/java/com/group4sweng/scranplan/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -28,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth.AuthStateListener mAuthListener;
     String mDisplayName;
 
+    private DrawerLayout drawer;
+
     TabLayout tabLayout;
     FrameLayout frameLayout;
     
@@ -38,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name, R.string.action_settings);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         initFirebase();
 
