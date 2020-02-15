3a4bc09537aa8840b0a7ce8b8fe32c207ce19dba
Jimb0nda <j.k.pearson87@gmail.com>
2020-02-15 15:13:12 +0000

    added dependencies

--- app/src/androidTest/java/com/group4sweng/scranplan/RecipeInfoTest.java
+++ app/src/androidTest/java/com/group4sweng/scranplan/RecipeInfoTest.java
@@ -7,17 +7,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.rule.ActivityTestRule;
 
 import com.google.android.material.tabs.TabLayout;
 
+import static org.junit.Assert.*;
 
 public class RecipeInfoTest {
 
     @Rule
-    public ActivityTestRule<RecipeInfoFragment> mActivityTestRule = new ActivityTestRule<RecipeInfoFragment>(RecipeInfoFragment.class);
+    public FragmentScenario<RecipeInfoFragment> myFragment;
 
-    private RecipeInfoFragment mActivity = null;
     private Button backButton;
     private TabLayout.Tab tabSwitch1;
     private TabLayout.Tab tabSwitch2;
@@ -25,15 +27,13 @@ public class RecipeInfoTest {
     @Before
     public void setUp() {
 
-        mActivity = mActivityTestRule.getActivity();
-        backButton = (Button)mActivity.findViewById(R.id.ReturnButton);
+
 
     }
 
     @After
     public void tearDown() {
 
-        mActivity = null;
 
     }
 
