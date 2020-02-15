06b419aa08a149a1777b00990f9d9565792d395c
Nathan Billis <nathanbillis@users.noreply.github.com>
2020-02-15 16:08:23 +0000

    Add Launch Screen and Login Features (A1 A2)
    * Added login ans logout functionality
    
    Also started JUnit 4 testing
    
    * JUnit testing for all login and register capabilities, all tests pass
    
    * Completed most of the JUnit testing for login
    
    checks input to buttons on screen
    
    * Revert "AI-3.5.2 <Administrator@WIN-7ELPGB9L6UA Create androidStudioFirstRun.xml, project.default.xml, laf.xml, notifications.xml, jdk.table.xml, Default.xml, debugger.xml, path.macros.xml, updates.xml"
    
    This reverts commit 893a6b0f463ab1015341866414b488a29029ba46.
    
    * added user info class and filled info on firestore with default on new user register
    
    Having some issues with the hashmap, once that is sorted these stories should be finished
    
    * Updated Login and UserInfo
    
    All tests complete - needs peer review
    
    * small adition
    
    * Updated login colours
    
    * Commented all classes and tests
    
    all tests run and pass
    
    Co-authored-by: Louis <49844440+louisifn@users.noreply.github.com>
    Co-authored-by: Shocksoc <shocksoc@yusu.org>

--- app/src/main/java/com/group4sweng/scranplan/MainActivity.java
+++ app/src/main/java/com/group4sweng/scranplan/MainActivity.java
@@ -13,16 +13,31 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import android.widget.FrameLayout;
 
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.*;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     final String TAG = "FirebaseTest";
+
+    UserInfo userDetails;
+    final FirebaseFirestore database = FirebaseFirestore.getInstance();
 
     FirebaseAuth mAuth;
     FirebaseApp mApp;
@@ -30,9 +45,10 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth.AuthStateListener mAuthListener;
     String mDisplayName;
 
+    Button mLogoutButton;
     TabLayout tabLayout;
     FrameLayout frameLayout;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         initFirebase();
+        initPageItems();
+        initPageListeners();
+
 
         tabLayout = findViewById(R.id.tabLayout);
         frameLayout = findViewById(R.id.frameLayout);
@@ -113,19 +132,62 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseUser user = mAuth.getCurrentUser();
 
                 if(user != null){
-                    Log.e(TAG, "AUTHENTICATION STATE UPDATE : Valid user logged in : email [" + user.getEmail() + "] and display name [" + user.getDisplayName() + "]");
-                    mDisplayName = user.getDisplayName();
+                    Log.e(TAG, "AUTHENTICATION STATE UPDATE : Valid user logged in : email [" + user.getEmail() + "]");
+
+                    DocumentReference usersRef = database.collection("users").document(mAuth.getCurrentUser().getUid());
+                    usersRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                if (task.getResult() == null) Log.d(TAG, "getResult is null");
+                                Log.d(TAG, "getResult: " + task.getResult());
+                                DocumentSnapshot document = task.getResult();
+                                HashMap<String, Object> map = new HashMap<>();
+
+                                map.put("UID", document.get("UID"));
+                                map.put("email", document.get("email"));
+                                map.put("displayName", document.get("displayName"));
+                                map.put("imageURL", document.get("imageURL"));
+                                map.put("chefRating", document.get("chefRating"));
+                                map.put("numRecipes", document.get("numRecipes"));
+                                map.put("preferences", document.get("preferences"));
+                                userDetails = new UserInfo(map, (HashMap<String, Object>) document.get("preferences"));
+                            }else {
+                                Log.e(TAG, "User details retrieval : Unable to retrieve user document in Firestore ");
+                                Toast.makeText(getApplicationContext(),"Unable to retrieve current user details, please sign in again.",Toast.LENGTH_SHORT).show();
+                                mAuth.signOut();
+                            }
+                        }
+                    });
                 }else{
                     Log.e(TAG,"AUTHENTICATION STATE UPDATE : No Valid current user logged in");
                     mDisplayName = "No Valid User";
 
                     Intent signIn = new Intent(getApplicationContext(), Login.class);
                     startActivity(signIn);
+                    userDetails = (UserInfo)signIn.getSerializableExtra("user");
                 }
             }
         };
         mAuth.addAuthStateListener(mAuthListener);
 
+
+    }
+
+    private void initPageItems(){
+        //Defining all relevant members of signin & register page
+        mLogoutButton = (Button) findViewById(R.id.logoutButton);
+    }
+
+    private void initPageListeners() {
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, "Logout button has been pressed and user has been logged out.");
+                userDetails = null;
+                mAuth.signOut();
+            }
+        });
 
     }
 
