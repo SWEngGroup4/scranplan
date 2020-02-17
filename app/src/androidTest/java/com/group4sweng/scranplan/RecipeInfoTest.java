523ee8b61b1a1c29c2822b9880fea9c7414945cf
Jimb0nda <j.k.pearson87@gmail.com>
2020-02-17 17:05:02 +0000

    Implemeting firebase

--- app/src/androidTest/java/com/group4sweng/scranplan/RecipeInfoTest.java
+++ app/src/androidTest/java/com/group4sweng/scranplan/RecipeInfoTest.java
@@ -2,27 +2,20 @@ package com.group4sweng.scranplan;
 
 import android.os.Handler;
 import android.os.Looper;
-import android.widget.Button;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import androidx.fragment.app.testing.FragmentScenario;
-import androidx.test.rule.ActivityTestRule;
 
-import com.google.android.material.tabs.TabLayout;
-
-import static org.junit.Assert.*;
 
 public class RecipeInfoTest {
 
     @Rule
     public FragmentScenario<RecipeInfoFragment> myFragment;
 
-    private Button backButton;
-    private TabLayout.Tab tabSwitch1;
-    private TabLayout.Tab tabSwitch2;
+
 
     @Before
     public void setUp() {
@@ -43,7 +36,6 @@ public class RecipeInfoTest {
             @Override
             public void run() {
 
-                backButton.callOnClick();
 
             }
 
--- app/src/main/java/com/group4sweng/scranplan/RecipeInfoFragment.java
+++ app/src/main/java/com/group4sweng/scranplan/RecipeInfoFragment.java
@@ -1,25 +1,67 @@
 package com.group4sweng.scranplan;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class RecipeInfoFragment extends AppCompatDialogFragment {
 
     Button mReturnButton;
     TabLayout mTabLayout2;
     FrameLayout mRecipeFrameLayout;
+    TextView mTitle;
+    ImageView mRecipeImage;
+    FirebaseAuth mAuth;
+
+    private FirebaseFirestore mDatabase;
+    private CollectionReference mDataRef;
+
+    // Define a String ArrayList for the ingredients
+    private ArrayList<String> ingredientList = new ArrayList<>();
+
+    // Define a ListView to display the data
+    private ListView listViewIngredients;
+
+    // Define an ArrayAdapter for the list
+    private ArrayAdapter<String> arrayAdapter;
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +75,32 @@ public class RecipeInfoFragment extends AppCompatDialogFragment {
         mReturnButton = layout.findViewById(R.id.ReturnButton);
         mTabLayout2 = layout.findViewById(R.id.tabLayout2);
         mRecipeFrameLayout = layout.findViewById(R.id.RecipeFrameLayout);
+        listViewIngredients = layout.findViewById(R.id.listViewText);
+        mTitle = layout.findViewById(R.id.Title);
+        mRecipeImage = layout.findViewById(R.id.recipeImage);
+
+//        mAuth = FirebaseAuth.getInstance();
+//        mAuth.getCurrentUser();
+//        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        mDatabase = FirebaseFirestore.getInstance();
+        mDataRef = mDatabase.collection("recipes");
+        DocumentReference docRef = mDataRef.document("wIHQ1GDdvJZ6jr4erzs4");
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+
+                    DocumentSnapshot document = task.getResult();
+                    Log.d("Recipe Name", ""+ document.getData().get("Name"));
+                    mTitle.setText(document.getData().get("Name").toString());
+
+
+                }
+            }
+        });
 
 
         initPageListeners();
@@ -82,6 +150,9 @@ public class RecipeInfoFragment extends AppCompatDialogFragment {
                 switch (tab.getPosition()) {
                     case 0:
                         fragment = new RecipeIngredientFragment();
+
+                        //dbData();
+
                         break;
                     case 1:
                         fragment = new RecipeCommentsFragment();
@@ -106,5 +177,48 @@ public class RecipeInfoFragment extends AppCompatDialogFragment {
 
 
     }
+
+    /*private void dbData(){
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mDataRef = mDatabase.getReference("recipes").child("wIHQ1GDdvJZ6jr4erzs4").child("Name");
+
+
+        listViewIngredients = getActivity().findViewById(R.id.listViewText);
+        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, ingredientList);
+        listViewIngredients.setAdapter(arrayAdapter);
+
+        mDataRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                String value = dataSnapshot.getValue(String.class);
+                ingredientList.add(value);
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+*/
+    //}
 
 }
