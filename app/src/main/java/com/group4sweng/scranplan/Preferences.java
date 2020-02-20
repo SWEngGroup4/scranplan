418b9bf4cb73c6e75f56f44ff7abe1e27ea876b3
Louis <lifnewman@gmail.com>
2020-02-20 14:11:11 +0000

    Linked to firestore to save preferences

---
+++ app/src/main/java/com/group4sweng/scranplan/Preferences.java
@@ -0,0 +1,296 @@
+package com.group4sweng.scranplan;
+
+/**
+ * Preferences class
+ * Used within UserInfo class to save specific preferences user wants to apply to all queries within
+ * the application
+ */
+public class Preferences {
+    // All preference variables
+    private boolean allergy_celery;
+    private boolean allergy_crustacean;
+    private boolean allergy_eggs;
+    private boolean allergy_fish;
+    private boolean allergy_gluten;
+    private boolean allergy_milk;
+    private boolean allergy_mustard;
+    private boolean allergy_nuts;
+    private boolean allergy_peanuts;
+    private boolean allergy_sesame;
+    private boolean allergy_shellfish;
+    private boolean allergy_soya;
+    private boolean allergy_sulphide;
+    private boolean diabetic;
+    private boolean halal;
+    private boolean high_protein;
+    private boolean kosher;
+    private boolean lactose_free;
+    private boolean lactovegetarian;
+    private boolean low_carb;
+    private boolean low_sodium;
+    private boolean no_alcohol;
+    private boolean no_pork;
+    private boolean ovovegetarian;
+    private boolean pescatarian;
+    private boolean vegan;
+    private boolean vegetarian;
+
+    // Constructor for all variables
+    Preferences(boolean allergy_celery,
+                boolean allergy_crustacean, boolean allergy_eggs, boolean allergy_fish,
+                boolean allergy_gluten, boolean allergy_milk, boolean allergy_mustard,
+                boolean allergy_nuts, boolean allergy_peanuts, boolean allergy_sesame,
+                boolean allergy_shellfish, boolean allergy_soya, boolean allergy_sulphide,
+                boolean diabetic, boolean halal, boolean high_protein, boolean kosher,
+                boolean lactose_free, boolean lactovegetarian, boolean low_carb,
+                boolean low_sodium, boolean no_alcohol, boolean no_pork,
+                boolean ovovegetarian, boolean pescatarian, boolean vegan,
+                boolean vegetarian){
+        this.allergy_celery = allergy_celery;
+        this.allergy_crustacean = allergy_crustacean;
+        this.allergy_eggs = allergy_eggs;
+        this.allergy_fish = allergy_fish;
+        this.allergy_gluten = allergy_gluten;
+        this.allergy_milk = allergy_milk;
+        this.allergy_mustard = allergy_mustard;
+        this.allergy_nuts = allergy_nuts;
+        this.allergy_peanuts = allergy_peanuts;
+        this.allergy_sesame = allergy_sesame;
+        this.allergy_shellfish = allergy_shellfish;
+        this.allergy_soya = allergy_soya;
+        this.allergy_sulphide = allergy_sulphide;
+        this.diabetic = diabetic;
+        this.halal = halal;
+        this.high_protein = high_protein;
+        this.kosher = kosher;
+        this.lactose_free = lactose_free;
+        this.lactovegetarian = lactovegetarian;
+        this.low_carb = low_carb;
+        this.low_sodium = low_sodium;
+        this.no_alcohol = no_alcohol;
+        this.no_pork = no_pork;
+        this.ovovegetarian = ovovegetarian;
+        this.pescatarian = pescatarian;
+        this.vegan = vegan;
+        this.vegetarian = vegetarian;
+    }
+
+    // Getters and setters for all variables
+
+    public boolean isAllergy_celery() {
+        return allergy_celery;
+    }
+
+    public void setAllergy_celery(boolean allergy_celery) {
+        this.allergy_celery = allergy_celery;
+    }
+
+    public boolean isAllergy_crustacean() {
+        return allergy_crustacean;
+    }
+
+    public void setAllergy_crustacean(boolean allergy_crustacean) {
+        this.allergy_crustacean = allergy_crustacean;
+    }
+
+    public boolean isAllergy_eggs() {
+        return allergy_eggs;
+    }
+
+    public void setAllergy_eggs(boolean allergy_eggs) {
+        this.allergy_eggs = allergy_eggs;
+    }
+
+    public boolean isAllergy_fish() {
+        return allergy_fish;
+    }
+
+    public void setAllergy_fish(boolean allergy_fish) {
+        this.allergy_fish = allergy_fish;
+    }
+
+    public boolean isAllergy_gluten() {
+        return allergy_gluten;
+    }
+
+    public void setAllergy_gluten(boolean allergy_gluten) {
+        this.allergy_gluten = allergy_gluten;
+    }
+
+    public boolean isAllergy_milk() {
+        return allergy_milk;
+    }
+
+    public void setAllergy_milk(boolean allergy_milk) {
+        this.allergy_milk = allergy_milk;
+    }
+
+    public boolean isAllergy_mustard() {
+        return allergy_mustard;
+    }
+
+    public void setAllergy_mustard(boolean allergy_mustard) {
+        this.allergy_mustard = allergy_mustard;
+    }
+
+    public boolean isAllergy_nuts() {
+        return allergy_nuts;
+    }
+
+    public void setAllergy_nuts(boolean allergy_nuts) {
+        this.allergy_nuts = allergy_nuts;
+    }
+
+    public boolean isAllergy_peanuts() {
+        return allergy_peanuts;
+    }
+
+    public void setAllergy_peanuts(boolean allergy_peanuts) {
+        this.allergy_peanuts = allergy_peanuts;
+    }
+
+    public boolean isAllergy_sesame() {
+        return allergy_sesame;
+    }
+
+    public void setAllergy_sesame(boolean allergy_sesame) {
+        this.allergy_sesame = allergy_sesame;
+    }
+
+    public boolean isAllergy_shellfish() {
+        return allergy_shellfish;
+    }
+
+    public void setAllergy_shellfish(boolean allergy_shellfish) {
+        this.allergy_shellfish = allergy_shellfish;
+    }
+
+    public boolean isAllergy_soya() {
+        return allergy_soya;
+    }
+
+    public void setAllergy_soya(boolean allergy_soya) {
+        this.allergy_soya = allergy_soya;
+    }
+
+    public boolean isAllergy_sulphide() {
+        return allergy_sulphide;
+    }
+
+    public void setAllergy_sulphide(boolean allergy_sulphide) {
+        this.allergy_sulphide = allergy_sulphide;
+    }
+
+    public boolean isDiabetic() {
+        return diabetic;
+    }
+
+    public void setDiabetic(boolean diabetic) {
+        this.diabetic = diabetic;
+    }
+
+    public boolean isHalal() {
+        return halal;
+    }
+
+    public void setHalal(boolean halal) {
+        this.halal = halal;
+    }
+
+    public boolean isHigh_protein() {
+        return high_protein;
+    }
+
+    public void setHigh_protein(boolean high_protein) {
+        this.high_protein = high_protein;
+    }
+
+    public boolean isKosher() {
+        return kosher;
+    }
+
+    public void setKosher(boolean kosher) {
+        this.kosher = kosher;
+    }
+
+    public boolean isLactose_free() {
+        return lactose_free;
+    }
+
+    public void setLactose_free(boolean lactose_free) {
+        this.lactose_free = lactose_free;
+    }
+
+    public boolean isLactovegetarian() {
+        return lactovegetarian;
+    }
+
+    public void setLactovegetarian(boolean lactovegetarian) {
+        this.lactovegetarian = lactovegetarian;
+    }
+
+    public boolean isLow_carb() {
+        return low_carb;
+    }
+
+    public void setLow_carb(boolean low_carb) {
+        this.low_carb = low_carb;
+    }
+
+    public boolean isLow_sodium() {
+        return low_sodium;
+    }
+
+    public void setLow_sodium(boolean low_sodium) {
+        this.low_sodium = low_sodium;
+    }
+
+    public boolean isNo_alcohol() {
+        return no_alcohol;
+    }
+
+    public void setNo_alcohol(boolean no_alcohol) {
+        this.no_alcohol = no_alcohol;
+    }
+
+    public boolean isNo_pork() {
+        return no_pork;
+    }
+
+    public void setNo_pork(boolean no_pork) {
+        this.no_pork = no_pork;
+    }
+
+    public boolean isOvovegetarian() {
+        return ovovegetarian;
+    }
+
+    public void setOvovegetarian(boolean ovovegetarian) {
+        this.ovovegetarian = ovovegetarian;
+    }
+
+    public boolean isPescatarian() {
+        return pescatarian;
+    }
+
+    public void setPescatarian(boolean pescatarian) {
+        this.pescatarian = pescatarian;
+    }
+
+    public boolean isVegan() {
+        return vegan;
+    }
+
+    public void setVegan(boolean vegan) {
+        this.vegan = vegan;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+
+}
--- app/src/main/java/com/group4sweng/scranplan/initialUserCustomisation.java
+++ app/src/main/java/com/group4sweng/scranplan/initialUserCustomisation.java
@@ -1,5 +1,6 @@
 package com.group4sweng.scranplan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,15 +9,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.Exclude;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Exclude;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class initialUserCustomisation extends AppCompatActivity {
     final String TAG = "userCustomisation";
@@ -24,6 +31,12 @@ public class initialUserCustomisation extends AppCompatActivity {
     FirebaseApp mApp;
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthStateListener;
+    UserInfo userDetails;
+    final FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+    Preferences preferences = userDetails.getPreferences();
+
+
 
     Button mSkipButton;
     CheckBox mVegetarianBox;
@@ -50,6 +63,14 @@ public class initialUserCustomisation extends AppCompatActivity {
         initPageItems();
 
         initPageListeners();
+    }
+
+    private void initFirebase(){
+        mApp = FirebaseApp.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+
+
     }
 
     private void initPageItems(){
@@ -80,16 +101,72 @@ public class initialUserCustomisation extends AppCompatActivity {
         });
 
     }
-@Exclude
-public  Map<String, Object> toMap() {
-    HashMap<String, Object> result = new HashMap<>();
 
-    if (mVegetarianBox.isChecked()){
-        result.put("vegetarian", true);
+    //make function to add info to firestore
+    private void savePref(){
+        DocumentReference usersRef =  database.document(mAuth.getCurrentUser().getUid());
+        HashMap<String, Object> preferences = new HashMap<>();
+
+        // Default user food preferences
+        preferences.put("allergy_celery", false);
+        preferences.put("allergy_crustacean", false);
+        preferences.put("allergy_eggs", false);
+        preferences.put("allergy_fish", false);
+        preferences.put("allergy_gluten", false);
+        preferences.put("allergy_milk", false);
+        preferences.put("allergy_mustard", false);
+        preferences.put("allergy_nuts", false);
+        preferences.put("allergy_peanuts", false);
+        preferences.put("allergy_sesame", false);
+        preferences.put("allergy_shellfish", false);
+        preferences.put("allergy_soya", false);
+        preferences.put("allergy_sulphide", false);
+        preferences.put("diabetic", false);
+        preferences.put("halal", false);
+        preferences.put("high_protein", false);
+        preferences.put("kosher", false);
+        preferences.put("lactose_free", false);
+        preferences.put("lactovegetarian", false);
+        preferences.put("low_carb", false);
+        preferences.put("low_sodium", false);
+        preferences.put("no_alcohol", false);
+        preferences.put("no_pork", false);
+        preferences.put("ovovegetarian", false);
+        preferences.put("pescatarian", false);
+        preferences.put("vegan", false);
+        preferences.put("vegetarian", false);
+
+
+        usersRef.update("preferences", preferences);
+        userDetails.updatePreferences(preferences);
     }
-    if (mVeganBox.isChecked()){
-        result.put("vegetarian", true);
+
+
+    //TODO need to add opening intent to this page after user is logged in or has returned from the user registration page
+
+    private void finishActivity() {
+
+        Log.e(TAG,"Initial User Customisation returning to main activity");
+
+        // User data returned to main menu
+        Intent returningIntent = new Intent();
+        returningIntent.putExtra("user", userDetails);
+        setResult(RESULT_OK, returningIntent);
+
+        finish();
     }
-}
+
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+
+        if (mVegetarianBox.isChecked()){
+            result.put("vegetarian", true);
+        }
+        if (mVeganBox.isChecked()){
+            result.put("vegetarian", true);
+        }
+    }
 
 }
