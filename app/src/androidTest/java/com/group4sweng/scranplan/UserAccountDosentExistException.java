3a9edcf1d1899d7975612dea27b9ac17e36ecca5
Joe Butler <profjb58@users.noreply.github.com>
2020-02-27 07:32:48 +0000

    Removed un-needed files

--- app/src/androidTest/java/com/group4sweng/scranplan/UserAccountDosentExistException.java
+++
@@ -1,7 +0,0 @@
-package com.group4sweng.scranplan;
-
-public class UserAccountDosentExistException extends Exception {
-    public UserAccountDosentExistException(String errorMessage, Throwable err){
-        super(errorMessage, err);
-    }
-}
