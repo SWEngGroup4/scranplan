fb5e52d718a27ac195629b9702152228a3d8e286
jamesclawley@gmail.com <CC3e$2fja2f6S@xP6!3Ae7i^LvF!Bh#Pp*W4fh7dew!cUzfeuDGtj4#5F&owveHW9zwexfgd>
2020-03-03 18:34:42 +0000

    Finished PrensentationTextView class - added demo in RecipeFragment

--- app/src/main/java/com/group4sweng/scranplan/PresentationTextView.java
+++ app/src/main/java/com/group4sweng/scranplan/PresentationTextView.java
@@ -1,39 +1,79 @@
 package com.group4sweng.scranplan;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.text.Html;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.core.provider.FontRequest;
 import androidx.core.provider.FontsContractCompat;
 
-// TODO - Need to confirm horizontal or vertical scroll box with other group
+import java.util.Timer;
+import java.util.TimerTask;
 
+@SuppressLint("ViewConstructor")
 public class PresentationTextView extends ScrollView {
 
-    LinearLayout.LayoutParams layoutParams;
+    private Handler mHandler = null;
+    private Timer startTimer = null;
+    private Timer endTimer = null;
+
+    LinearLayout.LayoutParams scrollLayoutParams;
     TextView textView;
     Integer slideHeight;
     Integer slideWidth;
-    Thread startTimer;
-    Thread endTimer;
 
     public PresentationTextView(Context context, Integer slideHeight, Integer slideWidth) {
         super(context);
 
-        layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        textView = new TextView(context);
+        scrollLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
+        textView = new TextView(context);
+        textView.setGravity(Gravity.CENTER);
+
+        LinearLayout linearLayout = new LinearLayout(context);
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        linearLayout.setGravity(Gravity.CENTER);
+//        linearLayout.setLayoutParams(linearLayoutParams);
+        linearLayout.addView(textView);
+
+        setFillViewport(true);
         this.slideHeight = slideHeight;
         this.slideWidth = slideWidth;
 
-        this.setLayoutParams(layoutParams);
-        this.addView(textView);
+        setLayoutParams(scrollLayoutParams);
+        addView(linearLayout);
+
+        // OPTIONAL - Uncomment code below if object is used within a ScrollView
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.performClick();
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+    }
+
+    public void setDims(Float width, Float height) {
+        scrollLayoutParams.width = Math.round(slideWidth * (width /  100));
+        scrollLayoutParams.height = Math.round(slideHeight * (height /  100));
+    }
+
+    public void setBackgroundColour(String colour) {
+        this.setBackgroundColor(Color.parseColor(colour));
     }
 
     public void setText(String text) {
@@ -41,11 +81,10 @@ public class PresentationTextView extends ScrollView {
     }
 
     public void setFont(String font) {
-        String query = font;
 
-        FontRequest fontRequest = new FontRequest("com.google.gms.fonts",
+        FontRequest fontRequest = new FontRequest("com.google.android.gms.fonts",
                 "com.google.android.gms",
-                query,
+                font,
                 R.array.com_google_android_gms_fonts_certs);
 
         FontsContractCompat.FontRequestCallback callback = new FontsContractCompat.FontRequestCallback() {
@@ -53,44 +92,78 @@ public class PresentationTextView extends ScrollView {
             public void onTypefaceRetrieved(Typeface typeface) {
                 textView.setTypeface(typeface);
             }
+            @Override
+            public void onTypefaceRequestFailed(int reason) {
+                Toast.makeText(getContext(), "Font request failed with exit code: " + reason, Toast.LENGTH_LONG).show();
+            }
         };
+        FontsContractCompat.requestFont(getContext(), fontRequest, callback, getThreadHandler());
     }
 
-    public void setSize(Integer size) {
+    public void setTextSize(Integer size) {
         textView.setTextSize(size);
     }
 
-    public void setColour(String colour) {
+    public void setTextColour(String colour) {
         textView.setTextColor(Color.parseColor(colour));
     }
 
     public void setPos(Float xPos, Float yPos) {
-        layoutParams.setMargins(Math.round(slideWidth* (xPos /  100)), Math.round(slideHeight * (yPos / 100)), 0 ,0);
+        scrollLayoutParams.setMargins(Math.round(slideWidth* (xPos /  100)), Math.round(slideHeight * (yPos / 100)), 0 ,0);
     }
 
-    // TODO - Need to confirm need for timers with other group
+    public void setStartTime(final Activity activity, Integer startTime) {
+        setVisibility(View.INVISIBLE);
+        Log.d("Test", "Timer started");
+        startTimer = new Timer();
+        startTimer.schedule(
+                new TimerTask() {
+                    @Override
+                    public void run() {
+                        activity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                setVisibility(View.VISIBLE);
+                            }
+                        });
+                    }
+                }, startTime
+        );
+    }
 
-//    public void appearAfter(final Integer startTime) {
-//        this.setVisibility(View.GONE);
-//        startTimer = new Thread() {
-//            @Override
-//            public void run() {
-//                try {
-//                    synchronized (this) {
-//                        wait (startTime);
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                textView.setVisibility(View.VISIBLE);
-//                            }
-//                        });
-//                    }
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        };
-//    }
+    public void setEndTime(final Activity activity, Integer endTime) {
+        endTimer = new Timer();
+        endTimer.schedule(
+                new TimerTask() {
+                    @Override
+                    public void run() {
+                        activity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                setVisibility(View.INVISIBLE);
+                            }
+                        });
+                    }
+                }, endTime
+        );
+    }
 
+    public void stopTimers() {
+        if (startTimer != null) {
+            startTimer.cancel();
+        }
+        if (endTimer != null) {
+            endTimer.cancel();
+        }
+    }
+
+    private Handler getThreadHandler() {
+        if (mHandler == null) {
+            HandlerThread handlerThread = new HandlerThread("fonts");
+            handlerThread.start();
+            mHandler = new Handler(handlerThread.getLooper());
+        }
+        return mHandler;
+    }
 
 }
