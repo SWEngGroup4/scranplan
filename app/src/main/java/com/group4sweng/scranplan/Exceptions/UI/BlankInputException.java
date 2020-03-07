cecca473a6e524094da793d32c356d1abf10621c
Joe Butler <profjb58@users.noreply.github.com>
2020-03-07 01:01:00 +0000

    Removed Blank Input error (Not needed)

--- app/src/main/java/com/group4sweng/scranplan/Exceptions/UI/BlankInputException.java
+++
@@ -1,9 +0,0 @@
-package com.group4sweng.scranplan.Exceptions.UI;
-
-public class BlankInputException extends Exception{
-
-    //  Tests if a text input field is blank or == null.
-    BlankInputException(String error){
-        super(error);
-    }
-}
