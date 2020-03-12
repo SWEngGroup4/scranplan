e1c29add11a96885ed091a6b2b4d59bcdb8f6626
Nathan Billis <nathanbillis@users.noreply.github.com>
2020-03-12 13:30:25 +0000

    Revert "A11 public profile (#24)" (#29)
    This reverts commit 15d87105172afafb2580cb0a47d5eeaa50e4e62b.

---
+++ app/src/androidTest/java/com/group4sweng/scranplan/RecipeInfoTest.java
@@ -0,0 +1,45 @@
+package com.group4sweng.scranplan;
+
+import android.os.Handler;
+import android.os.Looper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.fragment.app.testing.FragmentScenario;
+
+
+public class RecipeInfoTest {
+
+    @Rule
+    public FragmentScenario<RecipeInfoFragment> myFragment;
+
+
+
+    @Before
+    public void setUp() {
+
+
+
+    }
+
+    @After
+    public void tearDown() {
+
+
+    }
+
+    @Test
+    public void testRecipeInfo() {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+
+
+            }
+
+        });
+
+    }
+}
--- app/src/main/java/com/group4sweng/scranplan/Exceptions/InvalidUserException.java
+++
@@ -1,9 +0,0 @@
-package com.group4sweng.scranplan.Exceptions;
-
-public class InvalidUserException extends Exception{
-
-    //  Occurs when no valid user is found even after register/login. May happen if Wifi or data is off.
-    public InvalidUserException(String error){
-        super(error);
-    }
-}
