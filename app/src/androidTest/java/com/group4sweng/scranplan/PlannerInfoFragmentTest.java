645f5502cd3b6b2324475779cf0c701401eb9d51
Jimb0nda <j.k.pearson87@gmail.com>
2020-03-26 16:10:33 +0000

    Finished testing

--- app/src/androidTest/java/com/group4sweng/scranplan/PlannerInfoFragmentTest.java
+++ app/src/androidTest/java/com/group4sweng/scranplan/PlannerInfoFragmentTest.java
@@ -18,11 +18,13 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.action.ViewActions.pressBack;
 import static androidx.test.espresso.action.ViewActions.pressKey;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.group4sweng.scranplan.HomeTest.typeSearchViewText;
@@ -44,75 +46,79 @@ public class PlannerInfoFragmentTest {
         ActivityScenario.launch(Login.class);
 
         onView(withId(R.id.loginButton)).perform(click());
-        onView(withId(R.id.emailEditText)).perform(typeText("jamesclawley@gmail.com"));
-        onView(withId(R.id.passwordEditText)).perform(typeText("password"));
+        onView(withId(R.id.emailEditText)).perform(typeText("j.k.pearson87@gmail.com"));
+        onView(withId(R.id.passwordEditText)).perform(typeText("makaveli"));
         Espresso.closeSoftKeyboard();
 
         onView(withId(R.id.loginButton)).perform(click());
         Thread.sleep(THREAD_SLEEP_TIME);
     }
 
-    //Checks correct elements appear when add button is pressed
-    @Test
-    public void checkAddButton() {
-        Log.d(TAG, "Testing add button");
-
-        onView(withText("Meal Planner")).perform(click());
-        onView(withId(0)).perform(click());
-
-        onView(withId(R.id.menuSearch)).check(
-                matches(isDisplayed()));
-    }
-
-    //Checks searching for a recipe and adding it to the planner is performed correctly
+    /**
+     * Searches for a bacon sandwich and makes sure that all the information is there and that
+     * the icons can be clicked to show their data.
+     *
+     * This branch didn't have the meal planner saving system so meal planner refreshes each time, hence
+     * one big test method to test icons.
+     */
     @Test
     public void searchAndAddToPlanner() throws InterruptedException {
         Log.d(TAG, "Testing searching");
 
-        Thread.sleep(THREAD_SLEEP_TIME/4);
+        Thread.sleep(THREAD_SLEEP_TIME);
 
         onView(withText("Meal Planner")).perform(click());
         onView(withId(0)).perform(click());
 
-        Thread.sleep(THREAD_SLEEP_TIME/4);
+        Thread.sleep(THREAD_SLEEP_TIME);
 
         onView(withId(R.id.menuSearch)).perform(click());
-
-        Thread.sleep(THREAD_SLEEP_TIME/4);
 
         onView(isAssignableFrom(SearchView.class))
                 .perform(typeSearchViewText("bacon"))
                 .perform(pressKey(KeyEvent.KEYCODE_ENTER));
 
-        Thread.sleep(THREAD_SLEEP_TIME/4);
+        Thread.sleep(THREAD_SLEEP_TIME);
 
         onView(withText("Bacon Sandwich")).perform(click());
 
-        Thread.sleep(THREAD_SLEEP_TIME/4);
+        Thread.sleep(THREAD_SLEEP_TIME);
+
+        //Click image buttons and icons to test data is displayed from the searching screen
+
+        onView(withId(R.id.reheatInfoButton)).perform(click());
+
+        onView(isRoot()).perform(pressBack());
+
+        onView(withId(R.id.recipeInfoEggs)).perform(click());
+
+        onView(isRoot()).perform(pressBack());
+
+        onView(withId(R.id.recipeInfoWheat)).perform(click());
+
+        onView(isRoot()).perform(pressBack());
 
         onView(withText("Add")).perform(click());
+
+        onView(isRoot()).perform(pressBack());
+
+        //Click image buttons and icons to test data is displayed from the meal planner screen
+
+        onView(withId(0)).perform(click());
+
+        onView(withId(R.id.reheatInfoButton)).perform(click());
+
+        onView(isRoot()).perform(pressBack());
+
+        onView(withId(R.id.recipeInfoEggs)).perform(click());
+
+        onView(isRoot()).perform(pressBack());
+
+        onView(withId(R.id.recipeInfoWheat)).perform(click());
+
+        onView(isRoot()).perform(pressBack());
+
     }
-
-    //Checks meal planner saves for user after re-logging in
-//    @Test
-//    public void checkMealPlannerSaves() {
-//        Log.d(TAG, "Testing saving");
-//
-//        onView(withText("Meal Planner")).perform(click());
-//        onView(withId(0)).perform(click());
-//    }
-
-    //Checks long pressing a recipe removes it from the planner
-//    @Test
-//    public void removeItem() {
-//        Log.d(TAG, "Testing removal");
-//
-//        onView(withText("Meal Planner")).perform(click());
-//        onView(withId(0)).perform(longClick());
-//        onView(withId(0)).perform(click());
-//        onView(withId(R.id.menuSearch)).check(
-//                matches(isDisplayed()));
-//    }
 
     @After
     public void tearDown() { Log.d(TAG, "Tests complete"); }
