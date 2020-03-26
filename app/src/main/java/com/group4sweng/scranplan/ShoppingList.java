00ddb1a11fb68ccab1bd1f81fd793c392412c2d1
Becky anderson <becky.anderson219@gmail.com>
2020-03-26 11:49:21 +0000

    Update ShoppingList.java

--- app/src/main/java/com/group4sweng/scranplan/ShoppingList.java
+++ app/src/main/java/com/group4sweng/scranplan/ShoppingList.java
@@ -1,15 +1,32 @@
 package com.group4sweng.scranplan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.group4sweng.scranplan.UserInfo.FilterType;
+import com.group4sweng.scranplan.UserInfo.UserInfoPrivate;
+
+import java.util.ArrayList;
 
 public class ShoppingList extends AppCompatActivity implements FilterType {
 
     TextView mShoppingList;
+    private String UID;
+    private UserInfoPrivate mUserProfile;
+    private ArrayList<String> ingredientArray;
+
+    private ArrayList<String> ingredientList = new ArrayList<>();
+
+    private ListView listViewIngredients;
+
+    private ArrayAdapter<String> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,13 @@ public class ShoppingList extends AppCompatActivity implements FilterType {
         mShoppingList = findViewById(R.id.shoppingListText);
 
 
+    }
+    @Override
+    public void onBackPressed() {
+        Intent returnIntent = new Intent(this, Home.class);
+        returnIntent.putExtra("user", mUserProfile);
+        startActivity(returnIntent);
+        finish(); //    We don't need to send anything back but do need to destroy the current activity.
     }
 
 
