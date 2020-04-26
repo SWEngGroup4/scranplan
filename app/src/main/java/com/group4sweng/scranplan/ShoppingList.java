073cad8c2df674a9cf540d10164ae257e8783744
Becky anderson <becky.anderson219@gmail.com>
2020-04-26 16:44:03 +0100

    changed some features

--- app/src/main/java/com/group4sweng/scranplan/ShoppingList.java
+++ app/src/main/java/com/group4sweng/scranplan/ShoppingList.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,27 +22,24 @@ import com.group4sweng.scranplan.UserInfo.UserInfoPrivate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class ShoppingList extends AppCompatActivity {
 
     final String TAG = "ShoppingList";
-    UserInfoPrivate userDetails;
     final FirebaseFirestore database = FirebaseFirestore.getInstance();
 
     private FirebaseFirestore mDatabase = FirebaseFirestore.getInstance();
     private CollectionReference mUserRef = mDatabase.collection("users");
 
-    //List for storing current ingredients
     private List<HashMap<String, Object>> updateIngredientList = new ArrayList<>();
 
     //User information
     private com.group4sweng.scranplan.UserInfo.UserInfoPrivate mUser;
-    private SearchPrefs prefs;
 
     TextView mShoppingList;
-    private String UID;
-    private UserInfoPrivate mUserProfile;
     private List<HashMap<String, Object>> ShoppingList = new ArrayList<>();
 
     @Override
@@ -53,13 +51,8 @@ public class ShoppingList extends AppCompatActivity {
         mUser = (com.group4sweng.scranplan.UserInfo.UserInfoPrivate) getIntent().getSerializableExtra("user");
 
         if (mUser != null) {
-            AddIngredients();
-            TextView tv = (TextView) findViewById(R.id.textView2);
-            for (HashMap<String, Object> ingredients : ShoppingList) {
-                tv.append((CharSequence) updateIngredientList);
-            }
+            AddIngredients(2);
         }
-
         }
 
         @Override
@@ -70,20 +63,18 @@ public class ShoppingList extends AppCompatActivity {
             finish(); //    We don't need to send anything back but do need to destroy the current activity.
         }
 
-        public void AddIngredients(){
+        public void AddIngredients(int x){
 
             if (mUser != null) {
                 ShoppingList = mUser.getMealPlanner();
+
                 HashMap<String, Object> updateIngredientList = new HashMap<>();
-                    for (int x = 0; x <= mUser.getMealPlanner().size(); x++) {
-                        if (ShoppingList != null) {
-                            ShoppingList.get(x).get("ingredientList");
-                            updateIngredientList.put("ingredientList", mUser.getIngredientList());
-                        } else {
+
+                            Object newIngredient = ShoppingList.get(x).get("ingredientList");
+                            updateIngredientList.put("ingredient", newIngredient);
+                            System.out.println(updateIngredientList);
                             TextView tv = (TextView) findViewById(R.id.textView2);
-                            tv.append("Nothing in shopping list");
-                        }
-                    }
+                            tv.setText(updateIngredientList.toString());
 
             }
         }
