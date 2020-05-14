24242bfd1b295f293444006e94cd8dcf0dba1c5f
Becky anderson <becky.anderson219@gmail.com>
2020-05-14 17:06:37 +0100

    Changed to a recycler view for ease

--- app/src/main/java/com/group4sweng/scranplan/ShoppingList.java
+++ app/src/main/java/com/group4sweng/scranplan/ShoppingList.java
@@ -3,13 +3,17 @@ package com.group4sweng.scranplan;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -27,7 +31,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-public class ShoppingList extends AppCompatActivity {
+
+
+public class ShoppingList extends AppCompatActivity implements RecyclerViewAdaptor.ItemClickListener {
 
     final String TAG = "ShoppingList";
     final FirebaseFirestore database = FirebaseFirestore.getInstance();
@@ -42,57 +48,72 @@ public class ShoppingList extends AppCompatActivity {
 
     TextView mShoppingList;
     private List<HashMap<String, Object>> ShoppingList = new ArrayList<>();
+    RecyclerViewAdaptor adapter;
+    List<String> newList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shoppinglist);
+       setContentView(R.layout.activity_shoppinglist);
 
         mUser = (com.group4sweng.scranplan.UserInfo.UserInfoPrivate) getIntent().getSerializableExtra("user");
 
         if (mUser != null) {
-                    AddIngredients();
+            AddIngredients();
         }
+
     }
 
-        @Override
-        public void onBackPressed () {
-            Intent returnIntent = new Intent(this, Home.class);
-            returnIntent.putExtra("user", mUser);
-            startActivity(returnIntent);
-            finish(); //    We don't need to send anything back but do need to destroy the current activity.
-        }
+    @Override
+    public void onItemClick(View view, int position) {
+    if (newList != null) {
+        newList.remove(position);
+    }
+            adapter.notifyItemRemoved(position);
+            System.out.println(newList);
 
-        public void AddIngredients() {
-            for (int a = 0; a < 20; a++) {
-                if (mUser != null) {
-
-                    if (mUser.getMealPlanner().get(a) != null) {
-
-                            ShoppingList = mUser.getMealPlanner();
-                            HashMap<String, String> updateIngredientList = new HashMap<>();
-                            updateIngredientList = (HashMap<String, String>) ShoppingList.get(a).get("ingredientHashMap");
-                            assert updateIngredientList != null;
-                            ArrayList<String> ingredientArray = RecipeHelpers.convertToIngredientListFormat(updateIngredientList);
-
-                            for (String ingredient : ingredientArray) {
-                                if (updateIngredientList.containsValue(ingredient)) {
-                                    System.out.println("Already got one of thess in yo lsit ");
+    }
 
 
-                                } else
-                                    {
-                                    System.out.println("Already got one of thess in yo lsit ");
-                                        TextView tv = (TextView) findViewById(R.id.textView2);
-                                        tv.setTextSize(15);
-                                        tv.append(ingredient);
-                                }
+    public void AddIngredients() {
+        ArrayList<String> newList = new ArrayList<>();
+        for (int a = 0; a < 20; a++) {
+            if (mUser != null) {
 
+                if (mUser.getMealPlanner().get(a) != null) {
+
+                    ShoppingList = mUser.getMealPlanner();
+                    HashMap<String, String> updateIngredientList = new HashMap<>();
+                    updateIngredientList = (HashMap<String, String>) ShoppingList.get(a).get("ingredientHashMap");
+                    assert updateIngredientList != null;
+                    ArrayList<String> ingredientArray = RecipeHelpers.convertToIngredientListFormat(updateIngredientList);
+                    for (String ingredient : ingredientArray) {
+                        if (newList.contains(ingredient)){
+
+                            newList.remove(ingredient);
+                            newList.add("2x" + ingredient);
+                        }
+                        else {
+                            newList.add(ingredient);
                         }
                     }
                 }
             }
-
         }
+        System.out.println(newList);
+        RecyclerView recyclerView = findViewById(R.id.rvShoppingList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewAdaptor(this, newList);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed () {
+        Intent returnIntent = new Intent(this, Home.class);
+        returnIntent.putExtra("user", mUser);
+        startActivity(returnIntent);
+        finish(); //    We don't need to send anything back but do need to destroy the current activity.
+    }
 }
