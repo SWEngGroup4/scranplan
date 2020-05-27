8d6aeaafd8d62c6dcf1ec651929ec190de2ee2af
Louis <lifnewman@gmail.com>
2020-05-27 20:15:56 +0100

    comment change

--- app/src/androidTest/java/com/group4sweng/scranplan/SocialTest.java
+++ app/src/androidTest/java/com/group4sweng/scranplan/SocialTest.java
@@ -54,7 +54,7 @@ import static org.hamcrest.Matchers.allOf;
  *  - Reviews can be added to posts in the same way they are added to a particular recipe
  *
  *  -- USER STORY TESTS LINKED WITH ---
- *  C27
+ *  C28, C16, C31, C29 & C24
  */
 @RunWith(AndroidJUnit4.class)
 public class SocialTest implements Credentials  {
