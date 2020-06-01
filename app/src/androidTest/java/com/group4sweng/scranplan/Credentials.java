463bff6f36a36c09d15e5d3b5b67b5c93c16cb16
Jimb0nda <j.k.pearson87@gmail.com>
2020-06-01 18:47:29 +0100

    fixed suggestion box test

--- app/src/androidTest/java/com/group4sweng/scranplan/Credentials.java
+++ app/src/androidTest/java/com/group4sweng/scranplan/Credentials.java
@@ -2,8 +2,8 @@ package com.group4sweng.scranplan;
 
 public interface Credentials {
     //  Default test values.
-    String TEST_EMAIL = "ncab500+test@york.ac.uk";
-    String TEST_PASSWORD = "password1";
+    String TEST_EMAIL = "j.k.pearson87@gmail.com";
+    String TEST_PASSWORD = "makaveli";
 
     String TEST_EMAIL_ACC2 = "ncab500+test2@york.ac.uk";
     String TEST_PASSWORD_ACC2 = "password1";
